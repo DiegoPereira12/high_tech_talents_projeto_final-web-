58552e520201d00231ef3cb4c76b5d6657197011
unknown <diegoo.junior55@gmail.com>
2022-04-26 23:24:16 -0300

    importando projeto final web 1.2

--- App/app.py
+++ App/app.py
@@ -4,13 +4,15 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:diego@localhost:5432/imobiliaria_api"
+
+app.config['SECRET_KEY'] = "random string"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
 class InquilinosModel(db.Model):
     __tablename__ = 'inquilinos'
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, autoincrement=True, primary_key = True)
     nome = db.Column(db.String())
     cpf = db.Column(db.String())
     data_nascimento = db.Column(db.String())
@@ -18,75 +20,69 @@ class InquilinosModel(db.Model):
     def __init__(self, nome, cpf, data_nascimento):
         self.nome = nome        
         self.cpf = cpf
-        self.data_nascimento = data_nascimento        
+        self.data_nascimento = data_nascimento
+    
+    def __repr__(self):  
+      return f"<Proprietário: id {self.id}, nome {self.nome}, cpf {self.cpf}, data de nascimento {self.data_nascimento}>"        
 
-    #def __repr__(self):
-    #    return f"<InquilinosModel {self.id}: {self.nome}, nascida(o) em {self.data_de_nascimento}.>"                 
-
-    app.route("/")
+    @app.route('/')
     def index_inquilino():
-        return render_template("index_inquilino.html")
+        return render_template('index_inquilino.html', inquilinos = InquilinosModel.query.all())
 
-    app.route("/cadastrar_inquilino")
+    @app.route('/cadastro_inquilino')
     def cadastrar_inquilino():
-        return render_template("cadastro_inquilino.html")
-
-    @app.route("/cadastro_inquilino", methods=['GET', 'POST'])
+        return render_template('cadastro_inquilino.html', inquilinos = InquilinosModel.query.all())
+    
+    @app.route('/cadastro_inquilino', methods = ['GET', 'POST'])
     def cadastro_inquilino():
-        if request.method == "POST":
-            nome = request.form.get("nome")
-            cpf = request.form.get("cpf")
-            data_nascimento = request.form.get("data_nascimento")
+        if request.method == 'POST':
+            if not request.form['name'] or not request.form['cpf'] or not request.form['data_nascimento']:
+                flash('Por favor, preencha todos os campos')
+            else:
+                inquilinos = InquilinosModel(request.form['name'], request.form['cpf'], request.form['data_nasciemnto'])
 
-            if nome and cpf and data_nascimento:
-                inq = InquilinosModel(nome, cpf, data_nascimento)
-                db.session.add(inq)
+                db.session.add(inquilinos)
                 db.session.commit()
-        
-        return redirect(url_for("index_inquilino"))
 
-    @app.route("/lista_inquilino")
+                flash('Registro adicionado com sucesso')
+                return redirect(url_for('casdastro_inquilino'))
+        return render_template('index_inquilino.html')    
+
+    @app.route('/')
     def lista_inquilino():
-        inq = InquilinosModel.query.all()
-        return render_template("lista_inquilino.html", inq=inq)
+        return render_template('lista_inquilino.html', inquilinos = InquilinosModel.query.all())
 
-
-    @app.route("/excluir_inquilino/<int:id>")
+    @app.route('/excluir_inquilino/<int:id>')
     def excluir_inquilino(id):
-        inq = InquilinosModel.query.filter_by(_id=id).first()
-
-        db.session.delete(inq)
+        inquilinos = InquilinosModel.query.filter_by(_id=id).first()
+        db.session.delete(inquilinos)
         db.session.commit()
+        inquilinos = InquilinosModel.query.all()
 
-        inq = InquilinosModel.query.all()
+        return render_template('lista_inquilino.html', inquilinos = InquilinosModel.query())
 
-        return render_template("lista_inquilinos.html", inq=inq)
-
-    @app.route("/atualizar_inquilino/<int:id>", methods=['GET', 'POST'])
+    @app.route('/atualizar_inquilino/<int:id>', methods = ['GET', 'POST'])
     def atualizar_inquilino(id):
-        inq = InquilinosModel.query.filter_by(_id=id).first()
+        inquilinos = InquilinosModel.query.filter_by(_id=id).first()
 
         if request.method == 'POST':
-            nome = request.form.get("nome")
-            cpf = request.form.get("cpf")
-            data_nascimento= request.form.get("data_nascimento")
+            if not request.form['name'] or not request.form['cpf'] or not request.form['data_nasciemnto']:
+                flash('Por favor, preencha todos os campos')
+            else:
+                inquilinos = InquilinosModel(request.form['name'], request.form['cpf'], request.form['data_nasciemnto'])
 
-            if nome and cpf and data_nascimento:
-                inq = InquilinosModel(nome, cpf, data_nascimento)
-                db.session.add(inq)
+                db.session.add(inquilinos)
                 db.session.commit()
 
-                return redirect(url_for("lista_inquilino"))
+                flash('Registro adicionado com sucesso')
+                return redirect(url_for('lista_inquilino'))
+        return render_template('atualiza_inquilino.html')
 
-        return render_template("atualiza_inquilino.html", inq=inq)
-
-
-############################################################################
 
 class ImovelModel(db.Model):
     __tablename__ = 'imovel'
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, autoincrement=True, primary_key = True)
     logradouro = db.Column(db.String())
     cep = db.Column(db.String())
     bairro = db.Column(db.String())
@@ -99,73 +95,70 @@ class ImovelModel(db.Model):
         self.bairro = bairro
         self.cidade = cidade
         self.proprietario_id = proprietario_id
+    
+    def __repr__(self):
+        return f'<Imóvel: id {self.id}, logradouro {self.logradouro}, cep {self.cep}, bairro {self.bairro}, cidade {self.cidade}, proprietario_id {self.proprietario_id})>'
 
-    app.route("/")
+    @app.route('/')
     def index_imovel():
-        return render_template("index_imovel.html")
+        return render_template('index_imovel.html', imovel = ImovelModel.query.all())
 
-    app.route("/cadastrar_imovel")
+    @app.route('/')
     def cadastrar_imovel():
-        return render_template("cadastro_imovel.html")
+        return render_template('cadastro_imovel.html', imovel = ImovelModel.query.all())
 
-    @app.route("/cadastro_imovel", methods=['GET', 'POST'])
+    @app.route('/cadastro_imovel', methods = ['GET', 'POST'])
     def cadastro_imovel():
-        if request.method == "POST":
-            logradouro = request.form.get("logradouro")
-            cep = request.form.get("cep")
-            bairro = request.form.get("bairro")
-            cidade = request.form.get("cidade")
-            proprietario_id = request.form.get("proprietario_id")
-            
-            if logradouro and cep and bairro and cidade and proprietario_id:
-                imov = ImovelModel(logradouro, cep, bairro, cidade, proprietario_id)
-                db.session.add(imov)
+        if request.method == 'POST':
+            if not request.form['logradouro'] or not request.form['cep'] or not request.form['bairro'] or not request.form['cidade'] or not request.form['bairro'] or not request.form['proprietario_id']:
+                flash('Por favor, preencha todos os campos')
+            else:
+                imovel = ImovelModel(request.form['logradouro'], request.form['cep'], request.form['bairro'], request.form['cidade'], request.form['bairro'], request.form['proprietario_id'])
+
+                db.session.add(imovel)
                 db.session.commit()
-        
-        return redirect(url_for("index_imovel"))
-  
-    @app.route("/lista_imovel")
+
+                flash('Registro adicionado com sucesso')
+                return redirect(url_for('casdastro_imovel'))
+        return render_template('index_imovel.html') 
+               
+    @app.route('/')
     def lista_imovel():
-        imov = ImovelModel.query.all()
-        return render_template("lista_imovel.html", imov=imov)
+        return render_template('lista_imovel.html', imovel = ImovelModel.query.all())
 
-    @app.route("/excluir_imovel/<int:id>")
+    @app.route('/excluir_imovel/<int:id>')
     def excluir_imovel(id):
-        imov = ImovelModel.query.filter_by(_id=id).first()
+        imovel = ImovelModel.query.filter_by(_id=id).first()
 
-        db.session.delete(imov)
+        db.session.delete(imovel)
         db.session.commit()
 
-        imov = ImovelModel.query.all()
+        imovel = ImovelModel.query.all()
 
-        return render_template("lista_imovel.html", imov=imov)
+        return render_template('lista_imovel.html', imovel = ImovelModel.query.all())
 
-    @app.route("/atualizar_imovel/<int:id>", methods=['GET', 'POST'])
+    @app.route('/atualizar_imovel/<int:id>', methods=['GET', 'POST'])
     def atualizar_imovel(id):
-        imov = ImovelModel.query.filter_by(_id=id).first()
+        imovel = ImovelModel.query.filter_by(_id=id).first()
 
-        if request.method == "POST":
-            logradouro = request.form.get("logradouro")
-            cep = request.form.get("cep")
-            bairro = request.form.get("bairro")
-            cidade = request.form.get("cidade")
-            proprietario_id = request.form.get("proprietario_id")
-            
-            if logradouro and cep and bairro and cidade and proprietario_id:
-                imov = ImovelModel(logradouro, cep, bairro, cidade, proprietario_id)
-                db.session.add(imov)
+        if request.method == 'POST':
+            if not request.form['logradouro'] or not request.form['cep'] or not request.form['bairro'] or not request.form['cidade'] or not request.form['bairro'] or not request.form['proprietario_id']:
+                flash('Por favor, preencha todos os campos')
+            else:
+                imovel = ImovelModel(request.form['logradouro'], request.form['cep'], request.form['bairro'], request.form['cidade'], request.form['bairro'], request.form['proprietario_id'])
+
+                db.session.add(imovel)
                 db.session.commit()
 
-                return redirect(url_for("lista_imovel"))
+                flash('Registro adicionado com sucesso')
+                return redirect(url_for('lista_imovel'))
+        return render_template('atualiza_imovel.html')
 
-        return render_template("atualiza_imovel.html", imov=imov)
 
-
-#####################################################################################
 class ProprietarioModel(db.Model):
     __tablename__ = 'proprietario'
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer, autoincrement=True, primary_key = True)
     nome = db.Column(db.String())
     cpf = db.Column(db.String())
     data_nascimento = db.Column(db.String())
@@ -174,69 +167,70 @@ class ProprietarioModel(db.Model):
         self.nome = nome        
         self.cpf = cpf
         self.data_nascimento = data_nascimento
+    
+    def __repr__(self):  
+      return f"<Proprietário: id {self.id}, nome {self.nome}, cpf {self.cpf}, data de nascimento {self.data_nascimento}>"
 
-    app.route("/")
+    @app.route('/')
     def index_proprietario():
-        return render_template("index_proprietario.html")
+        return render_template('index_proprietario.html', proprietario = ProprietarioModel.query.all())
 
-    app.route("/cadastrar_proprietario")
+    @app.route('/')
     def cadastrar_index_proprietario():
-        return render_template("cadastro_proprietario.html") 
+        return render_template('cadastro_proprietario.html', proprietario = ProprietarioModel.query.all()) 
 
-    @app.route("/cadastro_proprietario", methods=['GET', 'POST'])
+    @app.route('/cadastro_proprietario', methods=['GET', 'POST'])
     def cadastro_proprietario():
-        if request.method == "POST":
-            nome = request.form.get("nome")
-            cpf = request.form.get("cpf")
-            data_nascimento = request.form.get("data_nascimento")
+        if request.method == 'POST':
+            if not request.form['name'] or not request.form['cpf'] or not request.form['data_nasciemnto']:
+                flash('Por favor, preencha todos os campos')
+            else:
+                inquilinos = ProprietarioModel(request.form['name'], request.form['cpf'], request.form['data_nasciemnto'])
 
-            if nome and cpf and data_nascimento:
-                inq = ProprietarioModel(nome, cpf, data_nascimento)
-                db.session.add(inq)
+                db.session.add(inquilinos)
                 db.session.commit()
-        
-        return redirect(url_for("index_proprietario"))
 
-    @app.route("/lista_proprietario")
+                flash('Registro adicionado com sucesso')
+                return redirect(url_for('casdastro_proprietario'))
+        return render_template('index_proprietario.html')
+
+    @app.route('/')
     def lista_proprietario():
-        prop = ProprietarioModel.query.all()
-        return render_template("lista_proprietario.html", prop=prop)
+        return render_template('lista_proprietario.html', proprietario = ProprietarioModel)
 
-    @app.route("/excluir_proprietario/<int:id>")
+    @app.route('/excluir_proprietario/<int:id>')
     def excluir_proprietario(id):
-        prop = ProprietarioModel.query.filter_by(_id=id).first()
+        proprietario = ProprietarioModel.query.filter_by(_id=id).first()
 
-        db.session.delete(prop)
+        db.session.delete(proprietario)
         db.session.commit()
 
-        prop = ProprietarioModel.query.all()
+        proprietario = ProprietarioModel.query.all()
 
-        return render_template("lista_proprietario.html", prop=prop)
+        return render_template("lista_proprietario.html", proprietario = ProprietarioModel)
 
-    @app.route("/atualizar_proprietario/<int:id>", methods=['GET', 'POST'])
+    @app.route('/atualizar_proprietario/<int:id>', methods=['GET', 'POST'])
     def atualizar_proprietario(id):
-        inq = ProprietarioModel.query.filter_by(_id=id).first()
+        proprietario = ProprietarioModel.query.filter_by(_id=id).first()
 
         if request.method == 'POST':
-            nome = request.form.get("nome")
-            cpf = request.form.get("cpf")
-            data_nascimento= request.form.get("data_nascimento")
+            if not request.form['name'] or not request.form['cpf'] or not request.form['data_nasciemnto']:
+                flash('Por favor, preencha todos os campos')
+            else:
+                proprietario = ProprietarioModel(request.form['name'], request.form['cpf'], request.form['data_nasciemnto'])
 
-            if nome and cpf and data_nascimento:
-                prop = ProprietarioModel(nome, cpf, data_nascimento)
-                db.session.add(prop)
+                db.session.add(proprietario)
                 db.session.commit()
 
-                return redirect(url_for("lista_proprietario"))
+                flash('Registro adicionado com sucesso')
+                return redirect(url_for('lista_proprietario'))
+        return render_template('atualiza_proprietario.html')
 
-        return render_template("atualiza_proprietario.html", prop=prop)
 
-
-##########################################################################
 class AluguelModel(db.Model):
     __tablename__ = 'aluguel'
 
-    id = db.Column(db.Integer, primary_key = True)
+    id = db.Column(db.Integer,autoincrement=True, primary_key = True)
     id_imovel = db.Column(db.Integer) 
     id_inquilino = db.Column(db.Integer) 
 
@@ -244,43 +238,64 @@ class AluguelModel(db.Model):
         self.id_imovel = id_imovel
         self.id_inquilino = id_inquilino
     
-    app.route("/")
-    def index_aluguel():
-        return render_template("index_aluguel.html")
-
-    app.route("/cadastrar_aluguel")
-    def cadastrar_aluguel():
-        return render_template("cadastro_aluguel.html")
-
-    @app.route("/cadastro_aluguel", methods=['GET', 'POST'])
-    def cadastro_aluguel():
-        if request.method == "POST":
-            id_imovel = request.form.get("id_imovel")
-            id_inquilino = request.form.get("id_inquilino")
-
-            if id_imovel and id_inquilino:
-                alug = AluguelModel(id_imovel, id_inquilino)
-                db.session.add(alug)
-                db.session.commit()
-        
-        return redirect(url_for("index_aluguel"))
+    def __repr__(self):
+      return f' <Aluguel: id {self.id}, id_imovel{self.id_imovel}, id_inquilino{self.inquilino_id}>'
     
-    @app.route("/lista_aluguel")
+    app.route('/')
+    def index_aluguel():
+        return render_template('index_aluguel.html', aluguel = AluguelModel.query.all())
+
+    @app.route('/')
+    def cadastrar_aluguel():
+        return render_template('cadastro_aluguel.html', aluguel = AluguelModel.query.all())
+
+    @app.route('/cadastro_aluguel', methods=['GET', 'POST'])
+    def cadastro_aluguel():
+        if request.method == 'POST':
+            if not request.form['id_imovel'] or not request.form['id_inquilino']:
+                flash('Por favor, preencha todos os campos')
+            else:
+                aluguel = AluguelModel(request.form['id_imovel'], request.form['id_inquilino'])
+
+                db.session.add(aluguel)
+                db.session.commit()
+
+                return redirect(url_for('index_aluguel'))
+        return render_template('index_aluguel.html')
+
+    @app.route('/')
     def lista_aluguel():
-        alug = AluguelModel.query.all()
-        return render_template("lista_aluguel.html", alug=alug)
+        return render_template('lista_aluguel.html', aluguel = AluguelModel.query.all())
 
-    @app.route("/excluir_aluguel/<int:id>")
+    @app.route('/excluir_aluguel/<int:id>')
     def excluir_aluguel(id):
-        alug = AluguelModel.query.filter_by(_id=id).first()
+        aluguel = AluguelModel.query.filter_by(_id=id).first()
 
-        db.session.delete(alug)
+        db.session.delete(aluguel)
         db.session.commit()
 
-        alug = AluguelModel.query.all()
+        aluguel = AluguelModel.query.all()
 
-        return render_template("lista_aluguel.html", alug=alug)
+        return render_template('lista_aluguel.html', aluguel = AluguelModel.query.all())
+        
+    @app.route('/atualizar_aluguel/<int:id>', methods=['GET', 'POST'])
+    def atualizar_aluguel(id):
+        aluguel = AluguelModel.query.filter_by(_id=id).first()
+
+        if request.method == 'POST':
+            if not request.form['id_imovel'] or not request.form['id_inquilino']:
+                flash('Por favor, preencha todos os campos')
+            else:
+                aluguel = AluguelModel(request.form['id_imovel'], request.form['id_inquilino'])
+
+                db.session.add(aluguel)
+                db.session.commit()
+
+                flash('Registro adicionado com sucesso')
+                return redirect(url_for('lista_aluguel'))
+        return render_template('atualiza_aluguel.html')
 
 if __name__ == '__main__':
-    app.run(debug=True) 
-
+    db.create_all() 
+    app.run(debug=True)
+  
